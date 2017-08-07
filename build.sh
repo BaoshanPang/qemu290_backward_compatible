569d8e09d1818d0bb5099d47fcf42d134bcc7491
bpang_ubuntu_vm <bpang@ubuntu.(none)>
2017-08-07 13:07:02 -0700

    build qemu with hacked libc.a

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-BLDDIR=`pwd`
-SRCDIR=`dirname $0`
-cd $SRCDIR
 git checkout -b v290_backward_compatible
 git submodule update --init pixman
 
-cd $BLDDIR
-$SRCDIR/configure --static --disable-system --enable-linux-user
+mkdir -p build
+cd build
+
+../configure --static --disable-system --enable-linux-user
 make
+
+LIBC=`readlink -f ../libc.a`
+all="aarch64  arm  armeb  i386  mips  mipsel  ppc  ppc64  x86_64"
+for q in $all;do
+    cd $q-linux-user
+    rm -f qemu-$q
+    make V=1 CFLAGS="-v" 2>&1|grep collect2 > y.sh
+    chmod +x y.sh
+    sed -i "s#-lc#$LIBC#" y.sh
+    ./y.sh
+    cd ..
+done
